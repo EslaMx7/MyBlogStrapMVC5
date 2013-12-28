a01ed906633dd1816853539894b5cb50faa5fc0d
eslamx7 <eslamx7@gmail.com>
2013-12-28 16:07:10 +0200

    Second Change , Add Comments , View Post

---
+++ MyBlogStrapMVC5/Controllers/PostsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlogStrapMVC5.Controllers
+{
+    public class PostsController : Controller
+    {
+        BlogEntities db;
+        public PostsController()
+        {
+            db = new BlogEntities();
+        }
+
+        //
+        // GET: /Posts/
+        public ActionResult Post(int? id)
+        {
+
+            var post = db.Posts.Where(p => p.ID == id).FirstOrDefault();
+            post.Views += 1;
+
+            ViewBag.Title = post.Title;
+            ViewBag.PostID = post.ID;
+            db.SaveChangesAsync().Wait();
+            return View(post);
+        }
+
+        [HttpPost]
+        public ActionResult Comment(int? id, FormCollection form)
+        {
+            
+            
+            var post = db.Posts.Where(p => p.ID == id).FirstOrDefault();
+            post.Comments.Add(new Comment
+            {
+                PostID = id ?? 0,
+                Author = form["txtCommentAuthor"],
+                CommentContent = form["txtCommentContent"],
+                PublishDate = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() 
+
+            });
+
+            db.SaveChangesAsync().Wait();
+            return RedirectToAction("Post", "Posts", new { id = id });
+        }
+    }
+}
